8590dc3a47697be2bd3606c907a5392588f9f2c5
Dewmal <dewmal@egreen.io>
2021-03-11 22:58:42 +0530

    remove all unwanted css

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,11 @@
-import { h } from 'preact';
 import { Link } from 'preact-router/match';
-import style from './style.css';
-
 const Header = () => (
-	<header class={style.header}>
+	<header>
 		<h1>Preact App</h1>
 		<nav>
-			<Link activeClassName={style.active} href="/">Home</Link>
-			<Link activeClassName={style.active} href="/profile">Me</Link>
-			<Link activeClassName={style.active} href="/profile/john">John</Link>
+			<Link href="/">Home</Link>
+			<Link href="/profile">Me</Link>
+			<Link href="/profile/john">John</Link>
 		</nav>
 	</header>
 );
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,8 +1,5 @@
-import { h } from 'preact';
-import style from './style.css';
-
 const Home = () => (
-	<div class={style.home}>
+	<div>
 		<h1>Home</h1>
 		<p>This is the Home component.</p>
 	</div>
