5781d8ba4f8edc3b38fa6c3066ff576c80cc1f0a
Dewmal <dewmal@egreen.io>
2021-03-12 10:38:18 +0530

    clear form

--- src/routes/week/components/TaskAddForm/index.js
+++ src/routes/week/components/TaskAddForm/index.js
@@ -1,13 +1,19 @@
 import { useContext, useEffect, useState } from "preact/hooks";
 import { WeekTask } from "../../../../store/models/WeekTask";
-import {globalContext} from '../../../../store/rootStore'
+import { globalContext } from "../../../../store/rootStore";
 
 const TaskAddForm = () => {
   const store = useContext(globalContext);
   const { weekTaskUiStore } = store;
 
-  const [task, setTask] = useState(new WeekTask());
+  const [task, setTask] = useState(null);
   const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    if (!task) {
+      setTask(new WeekTask());
+    }
+  }, [task]);
 
   useEffect(() => {
     if (task) {
@@ -16,14 +22,20 @@ const TaskAddForm = () => {
     }
   }, [task, title]);
 
+  const clearForm = () => {
+    setTask(null);
+    setTitle("");
+  };
+
   return (
     <form class="w-full max-w-sm">
       <div class="flex items-center border-b border-teal-500 py-2">
         <input
           class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
           type="text"
-          placeholder="Jane Doe"
-          aria-label="Full name"
+          placeholder="Task title"
+          aria-label="Task Title"
+          value={title}
           onInput={(e) => {
             setTitle(e.target.value);
           }}
@@ -31,11 +43,10 @@ const TaskAddForm = () => {
         <button
           class="flex-shrink-0 bg-green-500 hover:bg-green-700 border-green-500 hover:border-green-700 text-sm border-4 text-white py-1 px-2 rounded"
           type="button"
-          onClick={
-            ()=>{
-              weekTaskUiStore.addTask(task);
-            }
-          }
+          onClick={() => {
+            weekTaskUiStore.addTask(task);
+            clearForm();
+          }}
         >
           Add
         </button>
