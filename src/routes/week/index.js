2b34f43befb3e1f6e7c38982a422023dcc83500f
Dewmal <dewmal@egreen.io>
2021-03-12 08:07:57 +0530

    Add week start date componetn

--- src/routes/week/index.js
+++ src/routes/week/index.js
@@ -1,15 +1,25 @@
-import { h } from "preact";
+import { useState, useEffect } from "preact/hooks";
 import TaskAddForm from "./components/TaskAddForm";
 import TaskListView from "./components/TaskListView";
-// Note: `user` comes from the URL, courtesy of our router
+import moment from "moment";
+
+const WeekStartDateComponent = () => {
+  const [weekStartDate, setWeekStartDate] = useState();
+  useEffect(() => {
+    const today = moment();
+    const weekStartDateObj = today.startOf("week");
+    setWeekStartDate(weekStartDateObj.format("YYYY-DD-MM"));
+  }, []);
+  return <>{weekStartDate}</>;
+};
+
 const Weekly = ({ user }) => {
   console.log(user);
   return (
     <div>
       <h1>Weekly plan</h1>
-
+      <WeekStartDateComponent />
       <TaskAddForm />
-
       <TaskListView />
     </div>
   );
