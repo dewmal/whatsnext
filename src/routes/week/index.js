3732683f8926065ed723754e0581d34349887d1d
Dewmal <dewmal@egreen.io>
2021-03-12 07:47:06 +0530

    Add container

--- src/routes/week/index.js
+++ src/routes/week/index.js
@@ -5,9 +5,23 @@ const Weekly = ({ user }) => {
   return (
     <div>
       <h1>Weekly plan</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Button
-      </button>
+
+      <form class="w-full max-w-sm">
+        <div class="flex items-center border-b border-teal-500 py-2">
+          <input
+            class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            placeholder="Jane Doe"
+            aria-label="Full name"
+          />
+          <button
+            class="flex-shrink-0 bg-green-500 hover:bg-green-700 border-green-500 hover:border-green-700 text-sm border-4 text-white py-1 px-2 rounded"
+            type="button"
+          >
+            Add
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
